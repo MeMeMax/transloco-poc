542f882da4e82c65f2198d0a4c7ab53ebc0c4b50
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2023-03-03 15:03:53 +0100

    create web component in AppModule

--- apps/webcomponent/src/app/app.module.ts
+++ apps/webcomponent/src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import {
+  ApplicationRef,
+  DoBootstrap,
+  Injector,
+  isDevMode,
+  NgModule,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -15,4 +22,24 @@ import { appRoutes } from './app.routes';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule implements DoBootstrap {
+  constructor(injector: Injector) {
+    const webComponentSelector = 'webcomponent';
+
+    if (!customElements.get(webComponentSelector)) {
+      const component = createCustomElement(AppComponent, {
+        injector,
+      });
+
+      customElements.define(webComponentSelector, component);
+    }
+  }
+
+  ngDoBootstrap(app: ApplicationRef): void {
+    if (isDevMode()) {
+      const componentElement = document.createElement('webcomponent-root');
+      document.body.appendChild(componentElement);
+      app.bootstrap(AppComponent);
+    }
+  }
+}
