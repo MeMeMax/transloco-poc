1471db15eb480391c6cf965975ff77eb7dc57fde
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2023-03-03 13:25:28 +0100

    add transloco module to feature one

--- apps/showcase/src/app/feature-one/feature-one.module.ts
+++ apps/showcase/src/app/feature-one/feature-one.module.ts
@@ -6,11 +6,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslocoModule } from '@ngneat/transloco';
+
 import { FeatureOneRoutingModule } from './feature-one-routing.module';
 import { FirstComponent } from './components/first/first.component';
 
 @NgModule({
   declarations: [FirstComponent],
-  imports: [CommonModule, FeatureOneRoutingModule],
+  imports: [CommonModule, FeatureOneRoutingModule, TranslocoModule],
 })
 export class FeatureOneModule {}
