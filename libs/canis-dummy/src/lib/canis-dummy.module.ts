a1501cd79dd11125f6b6bae44e5d773d27c9edb2
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2023-03-03 14:15:13 +0100

    add translations for canis-dummy library

--- libs/canis-dummy/src/lib/canis-dummy.module.ts
+++ libs/canis-dummy/src/lib/canis-dummy.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslocoModule, TRANSLOCO_SCOPE } from '@ngneat/transloco';
+
 import { SheepCounterComponent } from './components/sheep-counter/sheep-counter.component';
-import { TranslocoModule } from '@ngneat/transloco';
+
+export const loader = ['de', 'en'].reduce((acc: any, lang) => {
+  acc[lang] = () => import(`./assets/i18n/${lang}.json`);
+  return acc;
+}, {});
 
 @NgModule({
   imports: [CommonModule, TranslocoModule],
   declarations: [SheepCounterComponent],
   exports: [SheepCounterComponent],
+  providers: [
+    {
+      provide: TRANSLOCO_SCOPE,
+      useValue: {
+        scope: 'canisDummy',
+        loader,
+      },
+    },
+  ],
 })
 export class CanisDummyModule {}
