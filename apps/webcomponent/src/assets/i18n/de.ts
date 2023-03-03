fb7258a6dac3d70cd31f91ffdc656fbb6114bd48
Maximilian Ridder <maximilian.ridder@weidmueller.com>
2023-03-03 20:01:02 +0100

    use library in webcomponent

--- apps/webcomponent/src/assets/i18n/de.ts
+++ apps/webcomponent/src/assets/i18n/de.ts
@@ -1,3 +1,4 @@
 export const de = {
   headline: 'Dies ist eine webcomponent',
+  sheepCounter: 'Sheep Counter in WebComponent',
 };
--- apps/webcomponent/src/assets/i18n/en.ts
+++ apps/webcomponent/src/assets/i18n/en.ts
@@ -1,3 +1,4 @@
 export const en = {
   headline: 'This is a webcomponent',
+  sheepCounter: 'Sheep counter in webcomponent',
 };
